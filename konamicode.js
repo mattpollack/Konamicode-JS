f785b1f5280179977022099fdf34aef2ccdd1ea5
Matthew Pollack <matt.pck@gmail.com>
2013-11-25 19:51:42 -0500

    Konami-Code implementation

---
+++ konamicode.js
@@ -0,0 +1,20 @@
+document.onkeydown = checkKey;
+var konamiCode = new Array (38, 38, 40, 40, 37, 39, 37, 39, 66, 65);
+var currentCode = new Array();
+ 
+//Konami Code Check
+function checkKey(e) {
+         e = e || window.event;
+ 
+        currentCode.push(e.keyCode);
+        i = currentCode.length - 1;
+ 
+        if (currentCode[i] == konamiCode[i]) {
+                if (konamiCode.length == currentCode.length) {
+                        //DO KONAMI AWESOMENESS
+                        currentCode = new Array();
+                }
+        } else {
+                currentCode = new Array();
+        }
+}
