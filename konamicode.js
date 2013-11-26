83d2cf7d32bc011a7c47991f9b56cf3b9a117b6d
Matthew Pollack <matt.pck@gmail.com>
2013-11-25 19:58:12 -0500

    Added seperate function for Konami

--- konamicode.js
+++ konamicode.js
@@ -4,17 +4,21 @@ var currentCode = new Array();
  
 //Konami Code Check
 function checkKey(e) {
-         e = e || window.event;
- 
-        currentCode.push(e.keyCode);
-        i = currentCode.length - 1;
- 
-        if (currentCode[i] == konamiCode[i]) {
-                if (konamiCode.length == currentCode.length) {
-                        //DO KONAMI AWESOMENESS
-                        currentCode = new Array();
-                }
-        } else {
-                currentCode = new Array();
+    e = e || window.event;
+
+    currentCode.push(e.keyCode);
+    i = currentCode.length - 1;
+
+    if (currentCode[i] == konamiCode[i]) {
+        if (konamiCode.length == currentCode.length) {
+            konamiAction();
+            currentCode = new Array();
         }
+    } else {
+        currentCode = new Array();
+    }
+}
+
+function konamiAction() {
+    //DO KONAMI AWESOMENESS
 }
